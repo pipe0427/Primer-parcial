158da42598a1425e231ad554889459d4d69d2d44
pipe0427 <pipe.04271@gmail.com>
2023-03-07 08:32:52 -0500

    primer commit

---
+++ src/main/java/com/parcial/parcial/controllers/EmpleadoController.java
@@ -0,0 +1,14 @@
+package com.parcial.parcial.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("empleado")
+public class EmpleadoController {
+
+    
+    
+
+}
---
+++ src/main/java/com/parcial/parcial/model/Empleado.java
@@ -0,0 +1,51 @@
+package com.parcial.parcial.model;
+
+public class Empleado {
+    
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String direccion;
+    private int telefono;
+
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    
+}
